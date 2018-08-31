dbc8439097d2e10a988d0af46783b78833b5deb7
jeremyvera <jeremy_vera@hotmail.fr>
2018-08-31 21:36:25 +0200

    test 2

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>test vue cli3</div>
+    <div>test vue cli-3</div>
   </div>
 </template>
 
